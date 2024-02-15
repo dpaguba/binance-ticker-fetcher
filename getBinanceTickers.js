6ee13760cc055bfd5f22255e02292feecaff2dac
Dmytro Pahuba <dpaguba@gmail.com>
2024-02-15 10:41:36 +0100

    feat: create getTradingPairs function and add getTickers function

--- getBinanceTickers.js
+++ getBinanceTickers.js
@@ -12,12 +12,28 @@ async function getTradingPairs() {
         const response = await axios.get(endpoint);
         const tradingPairs = response.data.symbols.map(pair => pair.symbol);
 
-        await fs.writeFile('binance_tickers.txt', tradingPairs.join('\n'));
+        //Define the quote currencies you want to use
+        const quoteCurrencies = ["USDT"];
+
+        getTickers(tradingPairs, quoteCurrencies);
 
         console.log('Tickers successfully saved.');
     } catch (error) {
         console.error('Error fetching Trading Pairs:', error.message);
     }
+}
+
+function getTickers(tradingPairs, quoteCurrencies) {
+    const allPairs = [];
+
+    for (let i = 0; i < quoteCurrencies.length; i++) {
+        const filteredPairs = tradingPairs.filter(pair => pair.endsWith(quoteCurrencies[i]));
+        allPairs.push(...filteredPairs);
+    }
+
+    fs.writeFile("binance_tickers.txt", allPairs.join('\n'))
+        .then(() => console.log(`All tickers successfully saved in ${"binance_tickers.txt"}.`))
+        .catch(error => console.error('Error saving tickers:', error.message));
 }
 
 getTradingPairs();
